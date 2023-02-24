4b35631ab7d5aa93f8c1fa17a8a5df4b6a46d867
hannab8 <hg3805@wayne.edu>
2023-02-23 22:12:57 -0500

    Added dependencies for firebase

--- app/src/main/java/com/example/testing_features/MainActivity.java
+++ app/src/main/java/com/example/testing_features/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
     }
 }
